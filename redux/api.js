73ec5be0e597a6bf4b17962389b10784322de70c
Arrikk <horpeyhermi@gmail.com>
2022-07-27 22:13:18 +0100

    mOd

--- redux/api.js
+++ redux/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const API = axios.create({ baseURL: 'https://certify.baremind.co.za' })
+const API = axios.create({ baseURL: 'https://certify.baremind.co.za/api' })
 
 API.interceptors.request.use((req) => {
   if (localStorage.getItem('profile')) {
